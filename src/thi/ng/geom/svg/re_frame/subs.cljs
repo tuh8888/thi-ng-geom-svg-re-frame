10cda77011c5062afd903b283e3dbf6d69228bee
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-06-02 19:24:33 -0600

    Formatting.

--- src/thi/ng/geom/svg/re_frame/subs.cljs
+++ src/thi/ng/geom/svg/re_frame/subs.cljs
@@ -2,14 +2,10 @@
   (:require [re-frame.core :refer [reg-sub subscribe]]
             [thi.ng.geom.svg.re-frame.core :as svg-rf]))
 
-
 (reg-sub ::view-box (fn [db [_ id]] (get-in db [::svg-rf/id id :view-box])))
 
 (reg-sub ::view-box-string
   (fn [[_ id] _] (subscribe [::view-box id]))
-  (fn [{:keys [x y width height
-              scale]} _]
-    (apply str
-      (interpose " "
-        [x y (* scale width)
-         (* scale height)]))))
+  ;; Convert view-box info to string representation
+  (fn [{:keys [x y width height scale]} _]
+    (apply str (interpose " " [x y (* scale width) (* scale height)]))))
